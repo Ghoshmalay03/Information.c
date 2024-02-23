16caafd7b0f64f8337125e9a0bebf77c47f76862
Malay Ghosh <150132631+Ghoshmalay03@users.noreply.github.com>
2024-02-24 01:27:05 +0530

    Add files via upload
    This is a dynamic c-program which will display the information provided by the user in a sequential manner.

---
+++ store_info.c
@@ -0,0 +1,28 @@
+//WAP to store your name, city, section, roll no. and CGPA in the variables and print them.
+#include<stdio.h>
+int main(void){
+	char string[]="Malay Ghosh";
+	char string_1[]="Phagwara";
+	char string_2[]="KT";
+	int roll_no;
+	float CGPA;
+	
+	printf("Enter your name: ");
+	scanf("%s", &string);
+	printf("Residence: ");
+	scanf("%s", &string_1);
+	printf("Section: ");
+	scanf("%s", &string_2);
+	printf("Roll no.: ");
+	scanf("%d", &roll_no);
+	printf("CGPA: ");
+	scanf("%f", &CGPA);
+	
+	printf("Myself %s\n", string);
+	printf("From %s\n", string_1);
+	printf("Section: %s\n", string_2);
+	printf("Roll_no: %d\n", roll_no);
+	printf("CGPA: %.1f\n", CGPA);
+	
+	return 0;
+}
